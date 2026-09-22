a3e44efa165bf8a0edfcc37e3ebb3651f08d8e67
quevedocandela <quevedocandela05@gmail.com>
2026-09-22 19:41:22 -0300

    se cambiaron aspectos del estilo

--- src/data/site.ts
+++ src/data/site.ts
@@ -71,7 +71,7 @@ export const experiencia = [
         titulo: "Bachiller en Comunicación",
         institucion: "ENSNA N°30 - Rojas, Buenos Aires",
         periodo: "Egresada en 2022",
-        descripcion: "No tiene que ver directamente con programación, pero fue ahí donde aprendí a comunicar ideas complejas de manera simple."
+        //descripcion: "No tiene que ver directamente con programación, pero fue ahí donde aprendí a comunicar ideas complejas de manera simple."
     },
 ];
 
